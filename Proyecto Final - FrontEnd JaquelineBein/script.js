d6982af8b5256f62ef27fe24e677ac5bfe64bcc9
j-bein <hablemosportugues2022@gmail.com>
2025-08-20 19:19:00 -0300

    Proyecto Final Front End

---
+++ Proyecto Final - FrontEnd JaquelineBein/script.js
@@ -0,0 +1,7 @@
+document.getElementById('darkModeBtn').addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
+
+document.getElementById('webmasterBtn').addEventListener('click', () => {
+  alert("Desarrollado por Jaque — Creadora apasionada por la cultura, el sabor y la tecnología.");
+});
